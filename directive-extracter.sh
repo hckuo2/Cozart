053354f5c04f9574de638cb1e1894b9083e6a982
Hsuan-Chi Kuo <hsuanchikuo@gmail.com>
2019-03-03 23:58:33 -0600

    fix

--- directive-extracter.sh
+++ directive-extracter.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-find $1 | xargs grep -n "#if\|#else\|#endif" | grep -v "ifndef" | sed 's/\/\*.*//' | sort -V
+find $1 -type f | xargs grep -n "#if\|#else\|#endif" | grep -v "ifndef" \
+    | sed 's/\/\*.*//' | sort -V
