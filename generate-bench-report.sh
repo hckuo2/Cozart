fcfb5b700998c63b44e6b4483eea19d742cb8c7d
Hsuan-Chi Kuo <hckuo2@illinois.edu>
2019-03-18 03:33:18 -0500

    update benchmark report script

--- generate-bench-report.sh
+++ generate-bench-report.sh
@@ -1,18 +1,34 @@
 #!/bin/bash
+apache() {
+    echo apache-vanilla apache-cozart
+    paste -d ' ' \
+        <(grep Requests benchresult.apache.vanilla.tmp | awk '{print $4}') \
+        <(grep Requests benchresult.apache.cozart.tmp | awk '{print $4}')
+}
 
-echo apache-vanilla apache-cozart \
-    redis-get-vanilla redis-get-cozart redis-set-vanilla redis-set-cozart \
-    memcached-get-vanilla memcached-get-cozart memcached-set-vanilla memcached-set-cozart
+nginx() {
+    echo nginx-vanilla nginx-cozart
+    paste -d ' ' \
+        <(grep Requests benchresult.nginx.vanilla.tmp | awk '{print $4}') \
+        <(grep Requests benchresult.nginx.cozart.tmp | awk '{print $4}')
+}
 
-paste -d ' ' \
-    <(grep Requests benchresult.apache.vanilla.tmp | awk '{print $4}') \
-    <(grep Requests benchresult.apache.cozart.tmp | awk '{print $4}') \
-    <(grep GET benchresult.redis.vanilla.tmp | cut -d',' -f2 | tr -d '"') \
-    <(grep GET benchresult.redis.cozart.tmp | cut -d',' -f2 | tr -d '"') \
-    <(grep SET benchresult.redis.vanilla.tmp | cut -d',' -f2 | tr -d '"') \
-    <(grep SET benchresult.redis.cozart.tmp | cut -d',' -f2 | tr -d '"') \
-    <(grep Gets benchresult.memcached.vanilla.tmp | awk '{print $2}') \
-    <(grep Gets benchresult.memcached.cozart.tmp | awk '{print $2}') \
-    <(grep Sets benchresult.memcached.vanilla.tmp | awk '{print $2}') \
-    <(grep Sets benchresult.memcached.cozart.tmp | awk '{print $2}')
+redis() {
+    echo redis-get-vanilla redis-get-cozart redis-set-vanilla redis-set-cozart
+    paste -d ' ' \
+        <(grep GET benchresult.redis.vanilla.tmp | cut -d',' -f2 | tr -d '"') \
+        <(grep GET benchresult.redis.cozart.tmp | cut -d',' -f2 | tr -d '"') \
+        <(grep SET benchresult.redis.vanilla.tmp | cut -d',' -f2 | tr -d '"') \
+        <(grep SET benchresult.redis.cozart.tmp | cut -d',' -f2 | tr -d '"')
+}
 
+memcached() {
+    echo memcached-get-vanilla memcached-get-cozart memcached-set-vanilla memcached-set-cozart
+    paste -d ' ' \
+        <(grep Gets benchresult.memcached.vanilla.tmp | awk '{print $2}') \
+        <(grep Gets benchresult.memcached.cozart.tmp | awk '{print $2}') \
+        <(grep Sets benchresult.memcached.vanilla.tmp | awk '{print $2}') \
+        <(grep Sets benchresult.memcached.cozart.tmp | awk '{print $2}')
+}
+
+paste -d ' ' <($1) <($2) <($3) <($4) <($5) <($6) <($7)
