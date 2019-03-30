334984baf8c061c9058b7caaa729b5683a6a9633
Hsuan-Chi Kuo <hckuo2@illinois.edu>
2019-03-30 07:23:24 -0500

    add stats.py

---
+++ stats.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import sys
+
+def improvement(a, b):
+    return (a-b)/b * 100
+
+cur = 0
+stats = [0]
+for line in sys.stdin:
+    line = line.strip()
+    if cur == 0:
+        stats *= len(line.split())
+        print(line)
+    else:
+        nums = line.split()
+        for i in range(len(nums)):
+            stats[i] += float(nums[i])
+    cur += 1
+
+
+
+for i in range(len(stats)):
+    stats[i] /= cur
+    if i == 0:
+        print("{:.2f}".format(stats[i]), end="")
+    else:
+        print(" {:.2f}".format(stats[i]), end="")
+        if i % 2 == 1:
+            print(" ({:.2f} %)".format(improvement(stats[i], stats[i-1])), end="")
+print()
