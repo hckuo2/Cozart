47d66ce4226e15a6e81aa5cba30173e132cf80e2
Hsuan-Chi Kuo <hckuo2@illinois.edu>
2019-04-08 15:32:00 -0500

    update line2filename.sh

--- line2filename-config.sh
+++ line2filename-config.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 awk -F ":" '{print $1}' | sort | uniq >touched-files.tmp
-awk -f substr-join.awk touched-files.tmp filename.db | sort | uniq
+tmp=$(mktemp)
+cut -d' ' -f1 filename.db > $tmp
+grep -o -F -f $tmp touched-files.tmp | sort -k 1,1 | uniq | join -j 1 - <(sort -k 1,1 filename.db) | cut -d' ' -f2 \
+    | sort | uniq
