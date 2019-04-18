1611afcbf2f9922f4dccab20e54e73d1fa7c84b4
Hsuan-Chi Kuo <hckuo2@illinois.edu>
2019-04-17 20:50:34 -0500

    update scripts

--- include-dep.py
+++ include-dep.py
@@ -2,7 +2,7 @@
 import kconfiglib
 import os
 import sys
-os.environ['srctree'] = "linux-4.18.0"
+os.environ['srctree'] = "linux-xenial"
 os.environ['SRCARCH'] = "x86"
 os.environ['ARCH'] = "x86"
 kconf = kconfiglib.Kconfig()
