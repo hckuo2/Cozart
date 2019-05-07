8416a1bf2459d6d08a20ef93b356975c20740ed1
Hsuan-Chi Kuo <hckuo2@illinois.edu>
2019-05-06 22:33:50 -0500

    fix assign-config-value

--- assign-config-value.py
+++ assign-config-value.py
@@ -1,34 +1,34 @@
 #!/usr/bin/env python3
 import sys
 
-
-def parse_db():
-    db = {}
-    isnotset = []
-    filterfile = sys.argv[1]
-    for line in open(filterfile):
-        line = line.strip()
-        if len(line) == 0:
-            continue
-        if line[0] == "#":
-            if "is not set" in line:
-                isnotset.append(line)
-            continue
-        words = line.split("=")
-        if len(words) < 2:
-            continue
-        db[words[0]] = words[1]
-    return db, isnotset
-
+def is_constant(str):
+    return str != "m" and str != "y" and str != "n"
 
 if __name__ == '__main__':
-    db, isnotset = parse_db()
-    targetfile = sys.argv[2]
-    for line in isnotset:
-        print(line)
-    for line in open(targetfile):
-        if line[0] == "#":
-            continue
+    # parse the vanilla config file
+    vanilla = {}
+    with open(sys.argv[1]) as f:
+        for line in f:
+            line = line.strip()
+            if len(line) == 0:
+                continue
+            if line[0] == "#":
+                if "is not set" in line:
+                    cols = line.split()
+                    vanilla[cols[1]] = "is not set"
+                    print(line)
+            else:
+                cols = line.split("=")
+                print(line)
+                if not is_constant(cols[1]):
+                    vanilla[cols[0]] = cols[1]
+                else:
+                    print(line)
+
+    with open(sys.argv[2]) as f:
+        for line in f:
+            if line[0] == "#":
+                continue
         conf = line.strip()
-        if conf in db:
-            print(conf + "=" + db[conf])
+        if conf in vanilla:
+            print(conf + "=" + vanilla[conf])
