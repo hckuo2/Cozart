effa9f9c215db96f8019d37f93eb341fb8ce5069
Hsuan-Chi Kuo <hckuo2@illinois.edu>
2019-05-06 22:45:33 -0500

    fix trace2modline

--- trace2modline.sh
+++ trace2modline.sh
@@ -5,7 +5,11 @@ distro=$1
 tmp=$(mktemp)
 
 get_module_file() {
-    find $vanillamoddir -type f | grep --fixed-strings "/$1.ko"
+    if ! find $vanillamoddir -type f | grep "/$1.ko" ;
+    then
+        mod=$(echo $1 | tr "_" "-")
+        find $vanillamoddir -type f | grep "/$mod.ko"
+    fi
 }
 
 get_module_name() {
@@ -32,6 +36,11 @@ do
     mod=$(echo $line | get_module_name)
     offset=$(echo $line | get_module_addr)
     modfile=$(get_module_file $mod)
-    echo $trace | tr " " "\n" | python3 ./offset_addr.py $offset | addrmod2lines $modfile
+    if [ -z "$modfile" ]
+    then
+        >&2 echo "Can not locate file for $mod"
+    else
+        echo $trace | tr " " "\n" | python3 ./offset_addr.py $offset | addrmod2lines $modfile
+    fi
 done<modules.tmp
 } | sort | uniq
