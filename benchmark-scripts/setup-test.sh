2f18b0475a82babffe9f51c77cc1aad3c1717f9f
Hsuan-Chi Kuo <hckuo2@illinois.edu>
2019-06-15 11:09:01 -0500

    add setup

--- benchmark-scripts/setup-test.sh
+++ benchmark-scripts/setup-test.sh
@@ -8,9 +8,9 @@ apache() {
     rm -rf $DIR/test-apache
     mkdir -p $DIR/test-apache
     cd $DIR/test-apache
-    wget https://www-eu.apache.org/dist//httpd/httpd-2.4.39.tar.gz
+    wget --no-clobber https://www-eu.apache.org/dist//httpd/httpd-2.4.39.tar.gz
     tar -zvxf httpd-2.4.39.tar.gz
-    wget https://www-eu.apache.org/dist/perl/mod_perl-2.0.10.tar.gz
+    wget --no-clobber https://www-eu.apache.org/dist/perl/mod_perl-2.0.10.tar.gz
     tar -zvxf mod_perl-2.0.10.tar.gz
     cd httpd-2.4.39
     ./configure --prefix=$DIR/test-apache/httpd/prefork --with-mpm=prefork
@@ -24,27 +24,29 @@ apache() {
 }
 
 php() {
+    cd $DIR
     apt install -y wget pkg-config libxml2-dev unzip libsqlite3-dev
     rm -rf $DIR/php-src
-    wget https://github.com/php/php-src/archive/PHP-7.2.zip
+    wget --no-clobber https://github.com/php/php-src/archive/PHP-7.2.zip
     unzip PHP-7.2
     mv php-src-PHP-7.2 php-src
     cd $DIR/php-src
+    ./buildconf
     ./configure
     make -j`nproc`
     cd $DIR
 }
 
 memcached() {
+    cd $DIR
     apt install -y wget unzip autotools
     rm -rf $DIR/memcached-src
-    wget https://github.com/memcached/memcached/archive/1.5.16.zip
+    wget --no-clobber https://github.com/memcached/memcached/archive/1.5.16.zip
     unzip 1.5.16.zip
     mv memcached-1.5.16 memcached-src
     cd $DIR/memcached-src
     ./autogen.sh
     ./configure
     make -j`nproc`
-    make test
     cd $DIR
 }
