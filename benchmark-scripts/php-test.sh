5ffe5081540c4d5887f120cc3dd9f0066c564452
Hsuan-Chi Kuo <hckuo2@illinois.edu>
2019-06-16 11:31:44 -0500

    not to send results to php testing

--- benchmark-scripts/php-test.sh
+++ benchmark-scripts/php-test.sh
@@ -5,6 +5,6 @@ reqcnt=5000
 source benchmark-scripts/general-helper.sh
 bootstrap
 mark_start;
-make -C benchmark-scripts/php-src test
+echo n | make -C benchmark-scripts/php-src test
 mark_end;
 write_modules
