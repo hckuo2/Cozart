0a82ebde4a6220145633ebde15346d75e1ca70b4
Hsuan-Chi Kuo <hckuo2@illinois.edu>
2019-06-17 19:49:55 -0500

    add progress mysql-test

--- benchmark-scripts/mysql-test.sh
+++ benchmark-scripts/mysql-test.sh
@@ -6,7 +6,7 @@ mark_start;
 mkdir -p /home/hckuo2/mysqltest
 chmod 777 -R /home/hckuo2/mysqltest
 cd /usr/lib/mysql-test
-runuser hckuo2 -c "./mysql-test-run --vardir=/home/hckuo2/mysqltest --force --suite-timeout=7200 --testcase-timeout=600"
+runuser hckuo2 -c "./mysql-test-run --vardir=/home/hckuo2/mysqltest --force --test-progress --suite-timeout=7200 --testcase-timeout=600"
 cd /
 write_modules
 mark_end;
