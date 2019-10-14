b9d29f9a86200c43f37a1d5fcc306d8838c0c801
Hsuan-Chi Kuo <hckuo2@illinois.edu>
2019-10-14 18:07:21 -0500

    udpate redis-test

--- benchmark-scripts/redis-test.sh
+++ benchmark-scripts/redis-test.sh
@@ -2,7 +2,8 @@
 source benchmark-scripts/general-helper.sh
 bootstrap;
 mark_start;
-make -C benchmark-scripts/redis-src/ test
+make -C benchmark-scripts/redis-src/ test test-sentinel
+benchmark-scripts/redis-src/runtest-cluster
 write_modules
 mark_end;
 
