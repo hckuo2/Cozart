c7754b6337923fa0cc18c92826f51947d16a32c6
Hsuan-Chi Kuo <hckuo2@illinois.edu>
2019-12-23 11:54:16 -0600

    remove gcov data

--- gcov-gprof-stats/memcached/debloated/bench/epoch1/gcov/crc32c.h
+++
@@ -1,9 +0,0 @@
-#ifndef CRC32C_H
-#define    CRC32C_H
-
-typedef uint32_t (*crc_func)(uint32_t crc, const void *buf, size_t len);
-crc_func crc32c;
-
-void crc32c_init(void);
-
-#endif    /* CRC32C_H */
--- gcov-gprof-stats/redis/debloated/bench/epoch1/gcov/release.h
+++
@@ -1,3 +0,0 @@
-#define REDIS_GIT_SHA1 "00000000"
-#define REDIS_GIT_DIRTY "0"
-#define REDIS_BUILD_ID "qemu-1570913088"
